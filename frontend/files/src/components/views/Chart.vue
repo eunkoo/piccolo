f3e3dc370462bd2b6ab3c2c6d04ace602aa75c57
ywseo <ywseo@pentasecurity.com>
2019-05-30 10:28:24 +0900

    5/30 first attendance statistics chart page modification
    * 로그인 후 초기 출결 통계차트 페이지에 모든 그룹을 표시하다록 수정
    * 조회 기간내의 모든 주일을 기준으로 데이터를 생성.

--- frontend/files/src/components/views/Chart.vue
+++ frontend/files/src/components/views/Chart.vue
@@ -7,12 +7,57 @@ export default {
       type: Object,
       default: null
     },
-    options: {
-      type: Object,
-      default: null
+    chartTitle: {
+      type: String, 
+      default: 'chart title',
     }
   },
   mounted () {
+    this.options = {
+      title: {
+        display: true, 
+        text: this.chartTitle,
+        fontSize: 15,
+      },
+      legend: {
+        display: true, 
+        position: 'right', 
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true, 
+              autoSkip: true, 
+
+            },
+            gridLines: {
+              display: true
+            }
+          }],
+          xAxes: [ {
+            ticks: {
+              autoSkip: true,
+              autoSkipPadding: 20,
+              maxRotation: 10,
+              minRotation: 0,
+              labelOffset: 0,
+              callback: function(value, index, values) {
+                
+                let words = value.split('-');
+                if ((index % 10) == 0 ){
+                  return [ words[0], `${words[1]}-${words[2]}`];
+                }
+                return [`${words[1]}-${words[2]}`];
+              }
+            },
+            gridLines: {
+              display: false
+            }
+          }]
+        },
+    };
     this.renderChart(this.chartdata, this.options)
   }
 }
